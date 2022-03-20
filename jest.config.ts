8fd7c4378262a6af63067619dd3951797e00cfe9
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-20 17:07:15 -0300

    chore: add jest env to eslint config

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,10 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testEnviroment: 'node',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
