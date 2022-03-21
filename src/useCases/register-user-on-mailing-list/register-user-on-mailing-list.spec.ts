c2209d34a906c7065f983f350c5c043757c59e53
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-21 19:36:12 -0300

    feat: user data interface

---
+++ src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,16 @@
+import { UserData } from "./user-data"
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    /*
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const response = await useCase.registerUserOnMailingList({ name, email })
+    const user = await repo.findUserByEmail(email)
+    expect(user.name).toBe(name)
+    */
+  })
+})
