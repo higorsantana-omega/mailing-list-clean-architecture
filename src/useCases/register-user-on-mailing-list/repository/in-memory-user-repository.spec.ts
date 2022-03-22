3bd9da0c8e33ab0ea2813a8d021660b0c411c2e5
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-22 19:57:47 -0300

    test: add and find user and check its existence

--- src/useCases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/useCases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In memory user repository', () => {
     const user = await userRepo.findUserByEmail('any@email.com')
     expect(user).toBeNull()
   })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any-name'
+    const email = 'any@email.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail(email)
+    expect(user.name).toBe(name)
+  })
 })
