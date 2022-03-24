af6f9f953220c0fe6a889f836f65a4629b68e285
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-24 20:05:04 -0300

    feat: validate emails empty domain part

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -7,7 +7,7 @@ export class Email {
     const [local, domain] = email.split('@')
     if (local.length > 64 || local.length === 0) return false
 
-    if (domain.length > 255) return false
+    if (domain.length > 255 || domain.length === 0) return false
     return true
   }
 }
