7475fb6478463609461c1f108400c76490bf3c63
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-24 20:08:37 -0300

    feat: validate emails with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -8,6 +8,10 @@ export class Email {
     if (local.length > 64 || local.length === 0) return false
 
     if (domain.length > 255 || domain.length === 0) return false
+
+    const domainParts = domain.split('.')
+    if (domainParts.some(part => part.length > 63)) return false
+
     return true
   }
 }
