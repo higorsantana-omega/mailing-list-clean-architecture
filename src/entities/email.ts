d3b4114db10bf3a27f4ab529dec798b113c2df05
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-23 21:08:22 -0300

    feat: validate local part larger than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,10 @@
 export class Email {
   static validate (email: string): boolean {
     if (!email) return false
+
+    const [local] = email.split('@')
+    if (local.length > 64) return false
+
     return true
   }
 }
