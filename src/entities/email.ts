ecc1b1aaf6eb69896af561355b0121c0bb103cee
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-23 21:13:27 -0300

    feat: validate emails with more than 320 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,6 +2,8 @@ export class Email {
   static validate (email: string): boolean {
     if (!email) return false
 
+    if (email.length > 320) return false
+
     const [local] = email.split('@')
     if (local.length > 64) return false
 
