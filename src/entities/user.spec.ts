a06609c137096fc765456bde4b553deeb8fde9b5
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-27 02:39:41 -0300

    test: create valid user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -21,4 +21,14 @@ describe('User domain entity', () => {
     const error = User.create({ name: invalidName, email: 'any@email.com' })
     expect(error).toEqual(left(new InvalidNameError()))
   })
+
+  test('should create user with valid data', () => {
+    const validName = 'any_name'
+    const validEmail = 'any@email.com'
+    const user: User = User
+      .create({ name: validName, email: validEmail }).value as User
+
+    expect(user.name.value).toEqual(validName)
+    expect(user.email.value).toEqual(validEmail)
+  })
 })
