2f8e0ff806def0f01dab6f663a233fbcf51a361e
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-31 21:32:46 -0300

    test: register valid user to mailing list

--- src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,9 +1,11 @@
-// import { UserData } from '../../entities/user-data'
+import { UserData } from '../../entities/user-data'
+import { RegisterUserOnMailingList } from '../register-user-on-mailing-list'
+import { UserRepository } from './ports/user-repository'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
 
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
-    // const users: UserData[] = []
-    /*
+    const users: UserData[] = []
     const repo: UserRepository = new InMemoryUserRepository(users)
     const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
@@ -11,6 +13,6 @@ describe('Register user on mailing list use case', () => {
     const response = await useCase.registerUserOnMailingList({ name, email })
     const user = await repo.findUserByEmail(email)
     expect(user.name).toBe(name)
-    */
+    expect(response.value.name).toBe(name)
   })
 })
