5681f86f26c7f725353bdf8e9e8b0616958acf15
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-03 19:35:01 -0300

    feat: web controller should return 500 when server raises

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,31 +1,35 @@
 import { UserData } from '@/entities'
-import { RegisterUserOnMailingList } from '@/useCases'
+import { UseCase } from '@/useCases/ports'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
-import { created, badRequest } from '@/web-controllers/utils'
+import { created, badRequest, serverError } from '@/web-controllers/utils'
 import { MissingParamError } from './errors/missing-param-error'
 
 export class RegisterUserController {
-  private readonly usecase: RegisterUserOnMailingList
+  private readonly usecase: UseCase
 
-  constructor (usecase: RegisterUserOnMailingList) {
+  constructor (usecase: UseCase) {
     this.usecase = usecase
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.name || !request.body.email) {
-      let missingParam = (!request.body.name) ? 'name ' : ''
-      missingParam += (!request.body.email) ? 'email' : ''
-      return badRequest(new MissingParamError(missingParam.trim()))
-    }
-    const userData: UserData = request.body
-    const response = await this.usecase.registerUserOnMailingList(userData)
+    try {
+      if (!request.body.name || !request.body.email) {
+        let missingParam = (!request.body.name) ? 'name ' : ''
+        missingParam += (!request.body.email) ? 'email' : ''
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
 
-    if (response.isRight()) {
-      return created(response.value)
-    }
+      if (response.isRight()) {
+        return created(response.value)
+      }
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
