2a489e3e0e5417e71024d36fe9a8fb8e090c758f
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-09 20:55:31 -0300

    feat: config register route

---
+++ src/main/config/routes.ts
@@ -0,0 +1,14 @@
+/* eslint-disable node/no-path-concat */
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+export default (app: Express): void => {
+  const router = Router()
+  readdirSync(join(__dirname, 'routes')).map(async file => {
+    return import(join(__dirname, 'routes', file))
+      .then(route => route.default(router))
+      .catch((error) => console.log(error))
+  })
+  app.use('/api', router)
+}
