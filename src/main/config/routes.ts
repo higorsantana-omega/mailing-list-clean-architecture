cb72dd9d180ac92ac68a4a59e25ed8b0eed92f9d
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-10 20:08:16 -0300

    refactor: move middleware and routes folders inside main folder

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,8 +5,8 @@ import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
-  readdirSync(join(__dirname, 'routes')).map(async file => {
-    return import(join(__dirname, 'routes', file))
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    return import(join(__dirname, '../routes', file))
       .then(route => route.default(router))
       .catch((error) => console.log(error))
   })
