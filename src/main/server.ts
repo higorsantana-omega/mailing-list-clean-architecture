693ba70520d4e2fa3eb817090c98359c4a0323aa
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-12 21:39:57 -0300

    feat: change url mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect(process.env.MONGO_URL)
+MongoHelper.connect('mongodb://127.0.0.1:35971/')
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(5000, () => {
