e41374e7bdda310a157b6d6cf109923a716d85b2
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-16 18:19:55 -0300

    feat: send email when name and email are valid

---
+++ src/useCases/send-email/send-email.ts
@@ -0,0 +1,27 @@
+import { UserData } from '@/entities'
+import { Either } from '@/shared'
+import { MailServiceError } from '../errors/mail-service-error'
+import { UseCase } from '../ports'
+import { EmailOptions, EmailService } from './ports/email-service'
+
+export class SendEmail implements UseCase {
+  private readonly emailOptions: EmailOptions
+  private readonly emailService: EmailService
+
+  constructor (emailOptions: EmailOptions, emailService: EmailService) {
+    this.emailOptions = emailOptions
+    this.emailService = emailService
+  }
+
+  async perform (userData: UserData): Promise<Either<MailServiceError, EmailOptions>> {
+    const greetings = `E aí <b> ${userData.name} </b>, beleza?`
+    const customizedHtml = greetings + '<br> <br>' + this.emailOptions.html
+    const emailInfo: EmailOptions = {
+      ...this.emailOptions,
+      to: userData.name + '<' + userData.email + '>',
+      html: customizedHtml
+    }
+
+    return await this.emailService.send(emailInfo)
+  }
+}
