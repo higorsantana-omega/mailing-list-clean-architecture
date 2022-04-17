25d4752cc8311e37d2ce77420a538067b8d10094
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-17 16:52:52 -0300

    test: handle invalid user data

--- test/useCases/send-email/send-email.spec.ts
+++ test/useCases/send-email/send-email.spec.ts
@@ -1,4 +1,4 @@
-import { Either, Right, right } from '@/shared'
+import { Either, Left, Right, right } from '@/shared'
 import { MailServiceError } from '@/useCases/errors/mail-service-error'
 import { EmailOptions, EmailService } from '@/useCases/send-email/ports/email-service'
 import { SendEmail } from '@/useCases/send-email/send-email'
@@ -41,5 +41,13 @@ describe('Send email to user', () => {
     const useCase = new SendEmail(mailOptions, mailServiceStub)
     const response = await useCase.perform({ name: toName, email: toEmail })
     expect(response).toBeInstanceOf(Right)
+  })
+
+  test('should not try to email with invalid email address', async () => {
+    const mailServiceStub = new MailServiceStub()
+    const useCase = new SendEmail(mailOptions, mailServiceStub)
+    const invalidEmail = 'invalid_email'
+    const response = await useCase.perform({ name: toName, email: invalidEmail })
+    expect(response).toBeInstanceOf(Left)
   })
 })
