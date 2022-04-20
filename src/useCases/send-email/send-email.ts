d7eea282e7b13063fd4e0b8ca5e0e442c4dee1a0
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-04-20 19:32:32 -0300

    feat: make use case work with users

--- src/useCases/send-email/send-email.ts
+++ src/useCases/send-email/send-email.ts
@@ -1,6 +1,5 @@
-import { User, UserData } from '@/entities'
-import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
-import { Either, left } from '@/shared'
+import { User } from '@/entities'
+import { Either } from '@/shared'
 import { MailServiceError } from '../errors/mail-service-error'
 import { UseCase } from '../ports'
 import { EmailOptions, EmailService } from './ports/email-service'
@@ -14,18 +13,13 @@ export class SendEmail implements UseCase {
     this.emailService = emailService
   }
 
-  async perform (userData: UserData):
-  Promise<Either<MailServiceError | InvalidNameError | InvalidEmailError, EmailOptions>> {
-    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(userData)
-    if (userOrError.isLeft()) return left(userOrError.value)
-
-    const user = userOrError.value
-
-    const greetings = `E aí <b> ${user.name} </b>, beleza?`
+  async perform (user: User):
+  Promise<Either<MailServiceError, EmailOptions>> {
+    const greetings = `E aí <b> ${user.name.value} </b>, beleza?`
     const customizedHtml = greetings + '<br> <br>' + this.emailOptions.html
     const emailInfo: EmailOptions = {
       ...this.emailOptions,
-      to: user.name + '<' + user.email + '>',
+      to: `${user.name.value} < ${user.email.value} >`,
       html: customizedHtml
     }
 
